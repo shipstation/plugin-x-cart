101356e3dbfe08bde012a75e19173379eeeb5eae
Ben Vera <contact@benjaminvera.com>
2023-07-06 11:55:43 -0500

    Updated module w/ instructions on how to install locally (mac) (#7)
    * updated module w/ instructions on how to install locally (mac)
    
    * add logs and missing company line back
    
    * add version_info folder
    
    * updated versioning
    
    * upated readme
    
    * fixed shipping info bug
    
    * changelog version update
    
    * removed DS_Store and added to gitignore
    
    * fix namespace issue
    
    * update version

--- modules/ShipStation/Api/src/ShipStationBundle.php
+++ modules/ShipStation/Api/src/ShipStationBundle.php
@@ -7,14 +7,18 @@
  * Nor may you transmit it or store it in any other website or other form of electronic retrieval system.
  */
 
-namespace XLite\Module\ShipStation\Api;
+declare(strict_types=1);
 
+namespace ShipStation\Api;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 /**
  * Module description
  *
  * @package XLite
  */
-abstract class Main extends \XLite\Module\AModule
+
+final class ShipStationBundle extends Bundle
 {
 
     /**
@@ -35,26 +39,6 @@ abstract class Main extends \XLite\Module\AModule
     public static function getModuleName() 
     {
         return 'ShipStation';
-    }
-
-    /**
-     * Get module major version
-     *
-     * @return string
-     */
-    public static function getMajorVersion() 
-    {
-        return '5.4';
-    }
-
-    /**
-     * Module version
-     *
-     * @return string
-     */
-    public static function getMinorVersion() 
-    {
-        return '1.2';
     }
 
     /**
